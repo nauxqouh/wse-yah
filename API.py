f44468bc424f1306feaa39ee139d93ef90460568
nauxqouh <110775130+nauxqouh@users.noreply.github.com>
2024-09-07 13:50:07 +0700

    Update API.py

--- API.py
+++ API.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import uvicorn
 from rag_llm import RAGPipelineSetup
@@ -8,9 +8,12 @@ app = FastAPI()
 # Cấu hình FastAPI với các thông số giống như trong ứng dụng Streamlit của bạn
 EMBEDDINGS_MODEL_NAME = "BAAI/bge-m3"
 QDRANT_URL = "https://88bb6378-66e7-49db-a5de-6bb17f0d664a.europe-west3-0.gcp.cloud.qdrant.io:6333"
+NGROK_STATIC_DOMAIN = "WSE"
+NGROK_TOKEN = "2knpVVzzj8s7zHdWN2HJa5CMKTm_3MR4C9ZFWL1uTX6Lr4zEe"
 HUGGINGFACE_API_KEY = "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"
 QDRANT_API_KEY = "cLGVHbp48h0CZayJIXdxVW-JJijODOKpBFlzIPm6nvLHxRE4B_nrFA"
-GROQ_API_KEY= "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"
+RERANKER_MODEL_NAME = "BAAI/bge-reranker-v2-m3"
+GROQ_API_KEY= "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"
 DATABASE_TO_COLLECTION = {
     "Trường Đại học Khoa học Tự nhiên": "US_vectorDB",
     "Trường Đại học Công nghệ Thông tin": "UIT_vectorDB",
@@ -39,5 +42,11 @@ async def query(prompt_request: PromptRequest):
     response = rag_pipeline.run(prompt_request.prompt)
     return {"response": response}
 
-if __name__ == "__main__":
+
+@app.head("/api/query")
+async def query_head(request: Request):
+    # Respond with a 200 OK status for HEAD requests
+    return {"status": "HEAD request successful"}
+
+if _name_ == "_main_":
     uvicorn.run(app, host="0.0.0.0", port=8000)
